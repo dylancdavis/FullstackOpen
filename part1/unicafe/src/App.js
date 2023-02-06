4297c292a7c7aabd50e74d97a3612c4c31d33bb5
dylancdavis <dylcdav@gmail.com>
2023-02-06 12:26:40 -0500

    Exercises 1.7, 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,18 +6,34 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <div>
+  const handleGood = () => setGood(good+1)
+  const handleNeutral = () => setNeutral(neutral+1)
+  const handleBad = () => setBad(bad+1)
+
+  return (<Statistics 
+    good={good}
+    neutral={neutral}
+    bad={bad}
+    handleGood={handleGood}
+    handleNeutral={handleNeutral}
+    handleBad={handleBad}
+  />)
+}
+
+const Statistics = ({good, neutral, bad, handleGood, handleNeutral, handleBad}) => (
+  <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good+1)}>good</button>
-      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
-      <button onClick={() => setBad(bad+1)}>bad</button>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {(good-bad)/(good+neutral+bad)}</p>
+      <p>positive {(good/(good+neutral+bad))} %</p>
     </div>
-  )
-}
+)
 
 export default App
