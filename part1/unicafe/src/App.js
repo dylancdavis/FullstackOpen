0a2c88b0b64fda1f0f8acf835f0e78b9a43ba22c
dylancdavis <dylcdav@gmail.com>
2023-02-06 12:49:03 -0500

    Exercise 1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -42,12 +42,16 @@ const Statistics = ({good, neutral, bad}) => {
   if (good || neutral || bad) {
     return (<>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good+neutral+bad} />
-      <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)} />
-      <StatisticLine text="positive" value={(good/(good+neutral+bad))+'%'} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good+neutral+bad} />
+          <StatisticLine text="average" value={(good-bad)/(good+neutral+bad)} />
+          <StatisticLine text="positive" value={((good*100)/(good+neutral+bad))+'%'} />
+        </tbody>
+      </table>
   </>)
   } else {
     return (<>
@@ -58,7 +62,10 @@ const Statistics = ({good, neutral, bad}) => {
 }
 
 const StatisticLine = ({text, value}) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 export default App
