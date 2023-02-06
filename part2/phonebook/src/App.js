5b054bd5c8473a5e7963af0998dc14c67a9dfaeb
dylancdavis <dylcdav@gmail.com>
2023-02-06 16:05:44 -0500

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(r => {
+        setPersons(r.data)
+        console.log(r.data);
+      })
+  }, [])
 
 
   const handleNameChange = e => {
@@ -79,7 +84,6 @@ const PersonForm = ({newName, newNumber, onNameChange, onNumberChange, onSubmit}
 const Numbers = ({filteredNames}) =>  (
   <>
     <h2>Numbers</h2>
-    <p>Search by Name</p>
     {filteredNames.map(p => <p key={p.name}>{p.name} {p.number}</p>)}
   </>
 )
