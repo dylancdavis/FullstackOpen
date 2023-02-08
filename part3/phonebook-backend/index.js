2502ac0ebeafab1270381865adca99739037fa65
dylancdavis <dylcdav@gmail.com>
2023-02-08 12:47:29 -0500

    Exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,15 +25,32 @@ const persons = [
     }
 ]
 
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
-
+// GET info
 app.get('/info', (request, response) => {
   const firstLine = `Phonebook has info for ${persons.length} people`
   const secondLine = new Date();
+  console.log('request info');
   response.send(`<p>${firstLine}</p><p>${secondLine}</p>`)
 })
+
+// GET all persons
+app.get('/api/persons', (request, response) => {
+  console.log('request for all persons');
+  response.json(persons)
+})
+
+// GET person by ID
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  console.log(`request made for ${id}`);
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 
 const PORT = 3001;
 
