86b5c60e777bdf888afbf5f44fb0b07160b3f298
dylancdavis <dylcdav@gmail.com>
2023-02-12 18:58:23 -0500

    Extract router into module

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 //const mongoUrl = 'mongodb://localhost/bloglist'
 const mongoUrl = process.env.MONGO_URI
@@ -14,23 +15,25 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
 
 const PORT = 3003
 app.listen(PORT, () => {
