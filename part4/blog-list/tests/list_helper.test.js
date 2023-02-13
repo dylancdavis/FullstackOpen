0d74683f5ee0e39749a39eb1efc9adaa64ecf8b9
dylancdavis <dylcdav@gmail.com>
2023-02-13 13:12:09 -0500

    Exercise 4.5

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,25 +1,109 @@
-const { dummy, totalLikes } = require('../utils/list_helper')
+const { dummy, totalLikes, favoriteBlog } = require('../utils/list_helper')
 
-// const dummy = list_helper.dummy
-// const totalLikes = list_helper
-
-test('dummy should be 1', () => {
-    expect(dummy([])).toBe(1)
+describe('Dummy function', () => {
+    test('should be 1', () => {
+        expect(dummy([])).toBe(1)
+    })
 })
 
-test('Empty list has no likes', () => {
-    expect(totalLikes([])).toBe(0)
+describe('Number of likes in list', () => {
+    test('is zero with empty list', () => {
+        expect(totalLikes([])).toBe(0)
+    })
+
+    test('with one blog is that blogs likes', () => {
+        const oneBlogList = [{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }]
+    
+        expect(totalLikes(oneBlogList)).toBe(5)
+    })
+
+    test('with multiple blogs is their sum of likes', () => {
+        const threeBlogList = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 7,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 3,
+                __v: 0
+            },
+
+        ]
+    
+        expect(totalLikes(threeBlogList)).toBe(15)
+    })
+
 })
 
-test('One blog list has that blogs number of likes', () => {
-    const oneBlogList = [{
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 5,
-        __v: 0
-    }]
+describe('Favorite blog in list', () => {
+    test('is undefined with empty list', () => {
+        expect(favoriteBlog([])).toBe(undefined)
+    })
 
-    expect(totalLikes(oneBlogList)).toBe(5)
+    test('with one blog is that blog', () => {
+        const oneBlogList = [{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }]
+    
+        expect(favoriteBlog(oneBlogList)).toBe(oneBlogList[0])
+    })
+
+    test('with multiple blogs is blog with most likes', () => {
+        const threeBlogList = [
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 5,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 7,
+                __v: 0
+            },
+            {
+                _id: '5a422aa71b54a676234d17f8',
+                title: 'Go To Statement Considered Harmful',
+                author: 'Edsger W. Dijkstra',
+                url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+                likes: 3,
+                __v: 0
+            },
+
+        ]
+        expect(favoriteBlog(threeBlogList)).toBe(threeBlogList[1])
+    })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,7 +2,14 @@ const dummy = blogs => 1
 
 const totalLikes = blogs => blogs.reduce((total,current) => total + current.likes, 0)
 
+const favoriteBlog = blogs => blogs.reduce((favorite,current) => {
+    return favorite.likes > current.likes 
+        ? favorite
+        : current
+}, blogs.length ? blogs[0] : undefined)
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
