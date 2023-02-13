be6c15b9ebce7c39f86c8fed47b7a2797bbe0d66
dylancdavis <dylcdav@gmail.com>
2023-02-13 12:57:40 -0500

    Exercise 4.4

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -1,7 +1,25 @@
-const list_helper = require('../utils/list_helper')
+const { dummy, totalLikes } = require('../utils/list_helper')
 
-const dummy = list_helper.dummy
+// const dummy = list_helper.dummy
+// const totalLikes = list_helper
 
 test('dummy should be 1', () => {
     expect(dummy([])).toBe(1)
+})
+
+test('Empty list has no likes', () => {
+    expect(totalLikes([])).toBe(0)
+})
+
+test('One blog list has that blogs number of likes', () => {
+    const oneBlogList = [{
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }]
+
+    expect(totalLikes(oneBlogList)).toBe(5)
 })
