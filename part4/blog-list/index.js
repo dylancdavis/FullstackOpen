adb844470079a1b5c5f5d3b8555376ad277447e4
dylancdavis <dylcdav@gmail.com>
2023-02-12 19:02:26 -0500

    Exercise 4.2

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,41 +1,17 @@
 const http = require('http')
 const express = require('express')
 const app = express()
-require('dotenv').config()
+const { PORT, MONGO_URI} = require('./utils/config')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
 const blogsRouter = require('./controllers/blogs')
 
-//const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = process.env.MONGO_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(MONGO_URI)
 
 app.use(cors())
 app.use(express.json())
-
 app.use('/api/blogs', blogsRouter)
 
-// app.get('/api/blogs', (request, response) => {
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog
-//     .save()
-//     .then(result => {
-//       response.status(201).json(result)
-//     })
-// })
-
-const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
