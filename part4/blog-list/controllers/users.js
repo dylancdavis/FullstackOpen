d07e9000f07ff2f5a90396101f07c4a3396dd9e8
dylancdavis <dylcdav@gmail.com>
2023-02-14 17:08:04 -0500

    Exercise 4.16

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -5,6 +5,15 @@ const User = require('../models/user')
 userRouter.post('/', async (req, res) => {
 
     const { username, name, password } = req.body;
+    const minlength = 3;
+
+    if (!username || !password) return res.status(400).json({error: 'Missing username or password in request'})
+    if (username.length < minlength) return res.status(400).json({error: `Username must be at least ${minlength} characters long`})
+    if (password.length < minlength) return res.status(400).json({error: `Password must be at least ${minlength} characters long`})
+
+    const userExists = await User.findOne({ username })
+    if (userExists) return res.status(400).json({error: 'Username already exists'})
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
