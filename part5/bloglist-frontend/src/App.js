ceef5d12dffe9c417413b3a0a0c578c15c3a4e51
dylancdavis <dylcdav@gmail.com>
2023-02-17 10:47:46 -0500

    Exercises 5.1, 5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,23 +1,76 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsernameChange = e => setUsername(e.target.value)
+  const handlePasswordChange = e => setPassword(e.target.value)
+
+  const handleLogin = async e => {
+    e.preventDefault()
+    console.log('login submit.');
+    console.log('username',username);
+    console.log('password',password);
+
+    try {
+      const user = await loginService.login({username, password})
+      window.localStorage.setItem('loggedInUser',user)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('login success', user);
+    } catch (e) {
+      console.log(e.name, e.message);
+      console.log('error: incorrect login');
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInUser')
+    setUser(null)
+  }
+
+
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    (async () => {
+      const newBlogs = await blogService.getAll()
+      setBlogs(newBlogs)
+    })()
+    const loggedInUser = window.localStorage.getItem('loggedInUser')
+    if (loggedInUser) setUser(loggedInUser)
   }, [])
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    user
+    ? (<div>
+        <button onClick={handleLogout}>logout</button>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>)
+    : (<div>
+        <h1>login</h1>
+        <form onSubmit={handleLogin}>
+          <label>
+            username
+            <input value={username} onChange={handleUsernameChange} />
+          </label><br></br>
+          <label>
+            password
+            <input value={password} onChange={handlePasswordChange} />
+          </label><br></br>
+          <button type='submit'>login</button>
+        </form>
+      </div>)
   )
 }
 
