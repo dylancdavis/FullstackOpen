3b11fa30aea42428a1ea53613568f70935dd9f0a
dylancdavis <dylcdav@gmail.com>
2023-03-15 11:57:12 -0400

    Exercises 7.4-7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -73,23 +73,30 @@ const CreateNew = (props) => {
     })
   }
 
+  const resetInputs = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' {...content} />
+          <input name='content' {...content.attributes} />
         </div>
         <div>
           author
-          <input name='author' {...author} />
+          <input name='author' {...author.attributes} />
         </div>
         <div>
           url for more info
-          <input name='info' {...info} />
+          <input name='info' {...info.attributes} />
         </div>
         <button>create</button>
+        <button type='button' onClick={resetInputs}>reset</button>
       </form>
     </div>
   )
--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -3,8 +3,12 @@ import { useState } from "react"
 const useField = type => {
 	const [value, setValue] = useState('')
 	const onChange = e => { setValue(e.target.value)}
+	const reset = () => setValue('')
 
-	return { value, type, onChange }
+	return {
+		attributes: { value, type, onChange},
+		reset
+	}
 }
 
 export { useField }
