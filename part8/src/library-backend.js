479ad335ce42b5e4230b21215e30045a55f20a3c
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-04-30 17:13:25 +0000

    Exercise 8.5

--- part8/src/library-backend.js
+++ part8/src/library-backend.js
@@ -109,7 +109,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -119,10 +119,16 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
+      let filteredBooks = books;
       if (args.author) {
-        return books.filter((b) => b.author === args.author);
+        filteredBooks = filteredBooks.filter((b) => b.author === args.author);
       }
-      return books;
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((b) =>
+          b.genres.includes(args.genre)
+        );
+      }
+      return filteredBooks;
     },
     allAuthors: () => authors,
   },
