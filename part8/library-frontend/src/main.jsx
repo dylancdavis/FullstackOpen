57d17644c108094246b5033174793662a7c68ad8
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-05-01 12:04:20 +0000

    Get basic querying online

--- part8/library-frontend/src/main.jsx
+++ part8/library-frontend/src/main.jsx
@@ -1,8 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { InMemoryCache, ApolloClient, gql } from '@apollo/client';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    allAuthors {
+      name
+    }
+  }
+`;
+
+client.query({ query }).then((res) => {
+  console.log(res.data);
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
