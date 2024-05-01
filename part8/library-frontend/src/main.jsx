cbf44cf447e4cd434f607653c88b2e764b8045c9
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-05-01 12:10:07 +0000

    Exercise 8.8

--- part8/library-frontend/src/main.jsx
+++ part8/library-frontend/src/main.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { InMemoryCache, ApolloClient, gql } from '@apollo/client';
+import { InMemoryCache, ApolloClient, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
-const query = gql`
-  query {
-    allAuthors {
-      name
-    }
-  }
-`;
-
-client.query({ query }).then((res) => {
-  console.log(res.data);
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
