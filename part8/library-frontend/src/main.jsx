aaad25379cc43ac83ed78a2cc6d833e42548d70e
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-06-12 15:40:16 +0000

    Fix URL for websocket connection

--- part8/library-frontend/src/main.jsx
+++ part8/library-frontend/src/main.jsx
@@ -21,7 +21,7 @@ const authLink = setContext((_, { headers }) => {
 });
 
 const httpLink = createHttpLink({ uri: 'http://localhost:4000' });
-const wsLink = new GraphQLWsLink(createClient({ url: 'ws//localhost:4000' }));
+const wsLink = new GraphQLWsLink(createClient({ url: 'ws://localhost:4000' }));
 
 const splitLink = split(
   ({ query }) => {
