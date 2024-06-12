c08e700a2f5adb911144614941e411a4e29083c7
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-06-12 13:17:42 +0000

    Update backend to prepare for websocket support

--- part8/graphql-server/src/library-backend.js
+++ part8/graphql-server/src/library-backend.js
@@ -1,14 +1,23 @@
 const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
-const mongoose = require('mongoose');
+const {
+  ApolloServerPluginDrainHttpServer,
+} = require('@apollo/server/plugin/drainHttpServer');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const path = require('node:path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const { User } = require('./schema.js');
 const { typeDefs } = require('./typeDefs.js');
 const { resolvers } = require('./resolvers.js');
+
+// To load process.env
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 mongoose
   .connect(process.env.MONGODB_URI, { dbName: 'library' })
@@ -19,22 +28,37 @@ mongoose
     console.log('Error connecting to MongoDB:', error.message);
   });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const start = async () => {
+  const app = express();
+  const httpServer = http.createServer(app);
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null;
-    if (auth && auth.startsWith('Bearer ')) {
-      const encodedToken = auth.substring(7);
-      const decodedToken = jwt.verify(encodedToken, process.env.JWT_SECRET);
-      const foundUser = await User.findById(decodedToken.id);
-      return { currentUser: foundUser };
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+  const apolloServer = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await apolloServer.start();
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(apolloServer, {
+      context: async ({ req, res }) => {
+        const auth = req ? req.headers.authorization : null;
+        if (auth && auth.startsWith('Bearer ')) {
+          const encodedToken = auth.substring(7);
+          const decodedToken = jwt.verify(encodedToken, process.env.JWT_SECRET);
+          const foundUser = await User.findById(decodedToken.id);
+          return { currentUser: foundUser };
+        }
+      },
+    })
+  );
+
+  httpServer.listen(4000, () =>
+    console.log('Server running: http://localhost:4000')
+  );
+};
+
+start();
