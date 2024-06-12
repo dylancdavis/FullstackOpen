cc70db5bde2cf0f0e6bad59d2459c41cbbd72774
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-06-12 13:42:50 +0000

    Add WebSocketServer to backend

--- part8/graphql-server/src/library-backend.js
+++ part8/graphql-server/src/library-backend.js
@@ -5,6 +5,9 @@ const {
 const { expressMiddleware } = require('@apollo/server/express4');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
+const { WebSocketServer } = require('ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
+
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
@@ -32,9 +35,24 @@ const start = async () => {
   const app = express();
   const httpServer = http.createServer(app);
 
+  const wsServer = new WebSocketServer({ server: httpServer, path: '/' });
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+  const serverCleanup = useServer({ schema }, wsServer);
+
   const apolloServer = new ApolloServer({
-    schema: makeExecutableSchema({ typeDefs, resolvers }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
   });
 
   await apolloServer.start();
