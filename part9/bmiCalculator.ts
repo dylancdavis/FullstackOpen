79d159711af04d64a901c39c18553b7780b3ccde
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-09-06 16:49:16 +0000

    Exercise 9.5

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,4 +1,4 @@
-function calculateBMI(heightInCm: number, weightInKg: number): String {
+export function calculateBMI(heightInCm: number, weightInKg: number): String {
   const bmi = weightInKg / (heightInCm / 100) ** 2;
   if (bmi < 18.5) return 'underweight';
   if (bmi < 25) return 'normal weight';
@@ -6,29 +6,31 @@ function calculateBMI(heightInCm: number, weightInKg: number): String {
   return 'obese';
 }
 
-if (process.argv.length !== 4) {
-  throw new Error(
-    `Incorrect number of arguments (${
-      process.argv.length - 2
-    }). Usage: npm run calculateBmi <height (cm)> <weight (kg)>`
-  );
+if (require.main === module) {
+  if (process.argv.length !== 4) {
+    throw new Error(
+      `Incorrect number of arguments (${
+        process.argv.length - 2
+      }). Usage: npm run calculateBmi <height (cm)> <weight (kg)>`
+    );
+  }
+
+  const [, , heightArg, weightArg] = process.argv;
+  let height: number, weight: number;
+
+  try {
+    height = Number(heightArg);
+    if (isNaN(height)) throw new Error();
+  } catch (e) {
+    throw new Error(`Height ${heightArg} could not be parsed as a number.`);
+  }
+
+  try {
+    weight = Number(weightArg);
+    if (isNaN(weight)) throw new Error();
+  } catch (e) {
+    throw new Error(`Weight ${weightArg} could not be parsed as a number.`);
+  }
+
+  console.log(calculateBMI(height, weight));
 }
-
-const [, , heightArg, weightArg] = process.argv;
-let height: number, weight: number;
-
-try {
-  height = Number(heightArg);
-  if (isNaN(height)) throw new Error();
-} catch (e) {
-  throw new Error(`Height ${heightArg} could not be parsed as a number.`);
-}
-
-try {
-  weight = Number(weightArg);
-  if (isNaN(weight)) throw new Error();
-} catch (e) {
-  throw new Error(`Weight ${weightArg} could not be parsed as a number.`);
-}
-
-console.log(calculateBMI(height, weight));
--- part9/index.ts
+++ part9/index.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import { calculateBMI } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  try {
+    const { height, weight } = req.query;
+    const numHeight = Number(height);
+    const numWeight = Number(weight);
+    if (isNaN(numHeight)) throw new Error('Could not parse height');
+    if (isNaN(numWeight)) throw new Error('Could not parse weight');
+    const bmi = calculateBMI(numHeight, numWeight);
+    res.status(200).json({ height, weight, bmi });
+  } catch (e) {
+    res.status(404).send({ error: `malformatted parameters. ${e}` });
+  }
 });
 
 const PORT = 3000;
