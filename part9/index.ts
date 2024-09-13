664186ed6b0abdc5e51918faff8a7a4ac56ff4dd
Cole D <104338788+dylancdavis@users.noreply.github.com>
2024-09-13 15:43:13 +0000

    Set up /exercises endpoint

--- part9/index.ts
+++ part9/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { calculateBMI } from './bmiCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -18,6 +19,23 @@ app.get('/bmi', (req, res) => {
     res.status(200).json({ height, weight, bmi });
   } catch (e) {
     res.status(404).send({ error: `malformatted parameters. ${e}` });
+  }
+});
+
+app.post('/exercises', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+  if (daily_exercises === undefined || target === undefined)
+    res.status(400).json({ error: 'parameters missing' });
+
+  try {
+    const dailyExercises = Array.from(daily_exercises as unknown[]);
+    const dailyNums: number[] = dailyExercises.map(Number);
+    const numTarget = Number(target);
+    const result = calculateExercises(dailyNums, numTarget);
+    res.json(result);
+  } catch {
+    res.status(400).json({ error: 'malformatted parameters' });
   }
 });
 
